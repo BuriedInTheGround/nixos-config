fd53e3fb6361d68830f9a1caedee6a263e8a8f07
BuriedInTheGround <simone99.as@gmail.com>
2020-10-02 11:49:05 +0200

    :tada: Initial commit

---
+++ configuration.nix
@@ -0,0 +1,219 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+let
+  zshCompletionsPlugin = pkgs.zsh-completions.overrideAttrs (oldAttrs: rec {
+    fixupPhase = ''
+      mkdir -p $out/share/zsh/plugins/zsh-completions
+      cp -r ./* $out/share/zsh/plugins/zsh-completions/
+    '';
+  });
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true; # Enables NetworkManager.
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.wlp1s0.useDHCP = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  # };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Rome";
+
+  # Nix Packages collection configuration.
+  nixpkgs.config = {
+    # Allow proprietary packages.
+    allowUnfree = true;
+
+    # Enable Flash in browsers.
+    firefox = {
+      enableAdobeFlash = true;
+    };
+    chromium = {
+      enablePepperFlash = true;
+    };
+
+    # Create an alias for the unstable channel.
+    packageOverrides = pkgs: {
+      unstable = import <nixos-unstable> {
+        # Pass the nixpkgs config to the unstable alias to ensure `allowUnfree = true;` is propagated.
+        config = config.nixpkgs.config;
+      };
+    };
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    ##### Terminal & Shell Customization  #####
+    oh-my-zsh
+    zsh
+
+    ##### Shell Tools #####
+    binutils
+    curl
+    man
+    nano
+    posix_man_pages
+    ripgrep
+    wget
+
+    ##### GUI Tools #####
+    gnome-menus
+    gnome3.dconf-editor
+    gnome3.gnome-shell-extensions
+    gnome3.gnome-tweaks
+    gnomeExtensions.arc-menu
+    gnomeExtensions.dash-to-panel
+    unstable.vscode
+
+    ##### Archive Tools #####
+    bzip2
+    gzip
+    lzma
+    unrar
+    unzip
+    zip
+
+    ##### Misc Tools #####
+    acpi
+
+    ##### Browsers #####
+    chromium
+    firefoxWrapper
+
+    ##### Media #####
+    lollypop
+    mpv
+    nomacs
+
+    ##### Docker #####
+    docker
+    docker-compose
+    docker-slim
+  ];
+
+  # Enable ZSH and set it as default shell.
+  users.defaultUserShell = pkgs.zsh;
+  environment.pathsToLink = [ "/share/zsh" ];
+  programs.zsh = {
+    enable = true;
+    syntaxHighlighting.enable = true;
+    ohMyZsh = {
+      enable = true;
+      customPkgs = [
+        pkgs.spaceship-prompt
+        pkgs.zsh-you-should-use
+        zshCompletionsPlugin
+      ];
+      plugins = [ "you-should-use" ];
+      theme = "spaceship";
+    };
+  };
+
+  # Enable ADB.
+  programs.adb.enable = true;
+
+  # Enable Docker.
+  virtualisation.docker.enable = true;
+
+  # Fonts.
+  fonts.fonts = with pkgs; [
+    fira-code
+    fira-code-symbols
+    noto-fonts
+    noto-fonts-emoji
+    noto-fonts-extra
+    powerline-fonts
+    (unstable.nerdfonts.override {
+      fonts = [ "Meslo" ];
+    })
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  #   pinentryFlavor = "gnome3";
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  networking.firewall.allowedTCPPorts = [ 57621 ]; # Needed for Spotify.
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  # services.xserver.layout = "us";
+  services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable touchpad support.
+  # services.xserver.libinput.enable = true;
+
+  # Enable the KDE Desktop Environment.
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome3.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.simone = {
+    isNormalUser = true;
+    home = "/home/simone";
+    description = "Simone Ragusa";
+    extraGroups = [ "wheel" "adbusers" "docker" "networkmanager" ]; # `wheel` enable ‘sudo’ for the user.
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.03"; # Did you read the comment?
+
+}
+
