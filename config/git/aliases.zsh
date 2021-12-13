a7373bf355726b353833adbcb87ea8c97e9291b8
Simone Ragusa <simone99.as@gmail.com>
2021-12-13 10:20:32 +0100

    bin,config/git/aliases: add `git-clone-bare` script
    A script for cloning repositories in bare mode in a way that is
    convenient for using worktrees.

--- config/git/aliases.zsh
+++ config/git/aliases.zsh
@@ -1,3 +1,7 @@
 #!/usr/bin/env zsh
 
 alias git='noglob git'
+
+# Custom script to clone repositories in bare mode in a way that is convenient
+# for using worktrees.
+alias git-clone-bare="$MY_NIXOS_BIN/git-clone-bare"
