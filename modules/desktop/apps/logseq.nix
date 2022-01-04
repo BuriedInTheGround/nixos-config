6e814e151ebcbea5d51134ed9a4e3cc700aef8e7
Simone Ragusa <simone99.as@gmail.com>
2022-01-04 11:38:55 +0100

    modules/desktop/apps: pin logseq to 0.5.6

--- modules/desktop/apps/logseq.nix
+++ modules/desktop/apps/logseq.nix
@@ -11,6 +11,54 @@ in {
   };
 
   config = mkIf cfg.enable {
-    user.packages = [ pkgs.unstable.logseq ];
+    user.packages = with pkgs.unstable; [
+      (stdenv.mkDerivation rec {
+        pname = "logseq";
+        version = "0.5.6";
+
+        src = fetchurl {
+          url = "https://github.com/logseq/logseq/releases/download/${version}/logseq-linux-x64-${version}.AppImage";
+          sha256 = "aLps/nfBBAUJN9bsuESv5aM4eLZofT3p2UtWhr+9vgw=";
+          name = "${pname}-${version}.AppImage";
+        };
+
+        appimageContents = appimageTools.extract {
+          name = "${pname}-${version}";
+          inherit src;
+        };
+
+        dontUnpack = true;
+        dontConfigure = true;
+        dontBuild = true;
+
+        nativeBuildInputs = [ makeWrapper ];
+
+        installPhase = ''
+          runHook preInstall
+
+          mkdir -p $out/bin $out/share/${pname} $out/share/applications
+          cp -a ${appimageContents}/{locales,resources} $out/share/${pname}
+          cp -a ${appimageContents}/Logseq.desktop $out/share/applications/${pname}.desktop
+
+          substituteInPlace $out/share/applications/${pname}.desktop \
+            --replace Exec=Logseq Exec=${pname} \
+            --replace Icon=Logseq Icon=$out/share/${pname}/resources/app/icons/logseq.png
+
+          runHook postInstall
+        '';
+
+        postFixup = ''
+          makeWrapper ${electron_15}/bin/electron $out/bin/${pname} \
+            --add-flags $out/share/${pname}/resources/app
+        '';
+
+        meta = with lib; {
+          description = "A local-first, non-linear, outliner notebook for organizing and sharing your personal knowledge base";
+          homepage = "https://github.com/logseq/logseq";
+          license = licenses.agpl3Plus;
+          platforms = [ "x86_64-linux" ];
+        };
+      })
+    ];
   };
 }
