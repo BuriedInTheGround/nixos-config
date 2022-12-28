c3b62c7101542c465778cb3556c4750f6c75932a
Simone Ragusa <hi@interrato.dev>
2022-12-29 00:00:20 +0100

    modules/develop: unpin Go version

--- modules/develop/go.nix
+++ modules/develop/go.nix
@@ -11,7 +11,7 @@ in {
   };
 
   config = mkIf cfg.enable {
-    user.packages = [ pkgs.go_1_18 ]; # TODO: update when possible.
+    user.packages = [ pkgs.go ];
 
     # Ensure that Go installed local binaries are accessible through PATH.
     env.GOPATH = "$(go env GOPATH)";
